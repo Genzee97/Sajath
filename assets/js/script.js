6984eb59491b558430a18e740a32fc9f191fc1a2
Genzee97 <genze1109@gmail.com>
2025-09-12 18:43:56 +0530

    Update script.js
    Changed typing text

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["MIS Executive", "Quality Control Expert", "Data Processing Expert"],
+    strings: ["Operations & MIS Executive"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
